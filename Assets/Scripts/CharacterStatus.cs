1ffd44c565fc3eb9a4a9dd65e2106b476e2e691b
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2024-03-27 11:55:55 -0300

    Life and mana regen

--- Assets/Scripts/CharacterStatus.cs
+++ Assets/Scripts/CharacterStatus.cs
@@ -7,32 +7,60 @@ public class CharacterStatus : MonoBehaviour
 {
 
     public string name = "None";
+
+    [Header("Health")]
     public int currentLife = 100;
     [HideInInspector] public int maximumLife = 100;
     public int currentMana = 50;
     [HideInInspector] public int maximumMana = 50;
+    public float regenTimeSeconds = 1.5f;
+    public int lifeRegen = 1;
+    public int manaRegen = 2;
+
+    [Header("Attack basis")]
     public float range = Constants.rangeDistanceOffset;
     public float attackSpeed = 0.3f;
     public float moveSpeed = 5;
 
-    public Dictionary<string, Dictionary<string, int>> status = new Dictionary<string, Dictionary<string, int>>();
+    [Header("Attributes")]
+    public Dictionary<string, Dictionary<string, int>> status = new Dictionary<string, Dictionary<string, int>>() {
+        { "strength", new Dictionary<string, int>() {
+            { "points", 10 },
+            { "bonus", 0 },
+            { "modifier", 0 }
+        }},
+        { "intelligence", new Dictionary<string, int>() {
+            { "points", 10 },
+            { "bonus", 0 },
+            { "modifier", 0 }
+        }},
+        { "agility", new Dictionary<string, int>() {
+            { "points", 10 },
+            { "bonus", 0 },
+            { "modifier", 0 }
+        }},
+        { "vitality", new Dictionary<string, int>() {
+            { "points", 10 },
+            { "bonus", 0 },
+            { "modifier", 0 }
+        }}
+    };
 
     private void Start()
     {
+        HUD.UpdateHealthbar();
+    }
 
-        void createNewStatus(string name)
+    public void StartRegen()
+    {
+        // Check a possibility to stop this
+        StartCoroutine(RecoverRegen());
+        IEnumerator RecoverRegen()
         {
-            status[name] = new Dictionary<string, int>();
-            status[name]["points"] = 10;
-            status[name]["bonus"] = 0;
-            status[name]["modifier"] = 0;
+            yield return new WaitForSeconds(regenTimeSeconds);
+            RecoverLifeAndMana(lifeRegen, manaRegen);
+            StartRegen();
         }
-
-        createNewStatus("strength");
-        createNewStatus("intelligence");
-        createNewStatus("agility");
-        createNewStatus("vitality");
-
     }
 
     public int GetStatus(string statusName)
@@ -60,6 +88,44 @@ public class CharacterStatus : MonoBehaviour
 
         if( status[statusName]["bonus"] < 0 )
             status[statusName]["bonus"] = 0;
+    }
+
+    public void RecoverLifeAndMana( int lifeValue, int manaValue)
+    {
+        RecoverLife(lifeValue);
+        RecoverMana(manaValue);
+    }
+
+    public void RecoverLife(int value)
+    {
+
+        if (currentLife >= maximumLife)
+            return;
+
+        if (value + currentLife > maximumLife)
+        {
+            value = maximumLife;
+        }
+
+        currentLife += value;
+        HUD.UpdateLifebar();
+
+    }
+
+    public void RecoverMana( int value )
+    {
+
+        if (currentMana >= maximumMana)
+            return;
+
+        if( value + currentMana> maximumMana)
+        {
+            value = maximumMana;
+        }
+
+        currentMana+= value;
+        HUD.UpdateLifebar();
+
     }
 
 }
